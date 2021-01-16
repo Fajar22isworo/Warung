8371ac0aaf26f172d505fd6125b76fb0f24a54a7
Fajar22isworo <fjrisworo@gmail.com>
2021-01-16 09:42:32 +0700

    Add project files.

---
+++ Jenis/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warung.Jenis
+{
+
+        public class Item
+        {
+            public string item { get; set; }
+            public double price { get; set; }
+
+            public Item(string item, double price)
+            {
+                this.item = item;
+                this.price = price;
+            }
+        }
+    
+}
